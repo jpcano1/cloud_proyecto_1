f8273b706f2414ebf8e60f4dfa13010f8b7f29db
Juan Pablo <jp.cano@uniandes.edu.co>
2021-02-11 22:29:17 -0500

    Mejoras y arreglos

--- backend/src/app.py
+++ backend/src/app.py
@@ -54,14 +54,14 @@ db.init_app(app)
 with app.app_context():
     db.create_all()
 
-if not os.path.exists(app.config["BANNERS_FOLDER"]):
-    os.makedirs(app.config["BANNERS_FOLDER"])
+if not os.path.exists("src/" + app.config["BANNERS_FOLDER"]):
+    os.makedirs("src/" + app.config["BANNERS_FOLDER"])
 
-if not os.path.exists(app.config["CONVERTED_AUDIOS_FOLDER"]):
-    os.makedirs(app.config["CONVERTED_AUDIOS_FOLDER"])
+if not os.path.exists("src/" + app.config["CONVERTED_AUDIOS_FOLDER"]):
+    os.makedirs("src/" + app.config["CONVERTED_AUDIOS_FOLDER"])
 
-if not os.path.exists(app.config["RAW_AUDIOS_FOLDER"]):
-    os.makedirs(app.config["RAW_AUDIOS_FOLDER"])
+if not os.path.exists("src/" + app.config["RAW_AUDIOS_FOLDER"]):
+    os.makedirs("src/" + app.config["RAW_AUDIOS_FOLDER"])
 
 # Files Creation
 @app.route("/src/static/raw_audios/<filename>", methods=["GET"])
